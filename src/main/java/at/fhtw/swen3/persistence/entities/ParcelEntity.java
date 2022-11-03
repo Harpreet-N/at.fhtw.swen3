6529f90774d5b349cffaf787aae57834bfe21737
Harpreet <harpreetneharyt@gmail.com>
2022-11-03 16:00:26 +0100

    -Create ParcelEntity -Add dependencies

--- src/main/java/at/fhtw/swen3/persistence/entities/ParcelEntity.java
+++ src/main/java/at/fhtw/swen3/persistence/entities/ParcelEntity.java
@@ -1,4 +1,47 @@
 package at.fhtw.swen3.persistence.entities;
 
+import at.fhtw.swen3.services.dto.HopArrival;
+import at.fhtw.swen3.services.dto.Recipient;
+import at.fhtw.swen3.services.dto.TrackingInformation;
+
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
 public class ParcelEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "native")
+    @Column
+    private int id;
+
+    @Column
+    private Float weight;
+
+    @Column
+    private RecipientEntity recipient;
+
+    @Column
+    private RecipientEntity sender;
+
+    @Column
+    private String trackingId;
+
+    @Column
+    private TrackingInformation.StateEnum state;
+
+    @Column
+    private List<HopArrivalEntity> visitedHops = new ArrayList<>();
+
+    @Column
+    private List<HopArrivalEntity> futureHops = new ArrayList<>();
+
 }
