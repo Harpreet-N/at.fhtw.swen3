c251655a953c62298064c0de62462bc34b80c38b
Harpreet <harpreetneharyt@gmail.com>
2022-11-23 15:00:26 +0100

    -preparation of the tests

--- src/test/java/at/fhtw/swen3/controller/rest/ParcelApiControllerTest.java
+++ src/test/java/at/fhtw/swen3/controller/rest/ParcelApiControllerTest.java
@@ -1,11 +1,18 @@
 package at.fhtw.swen3.controller.rest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@SpringBootTest
+@TestPropertySource("application.properties")
 class ParcelApiControllerTest {
 
+/*
     @Test
     public void emptyTest(){
 
     }
+
+ */
 }
