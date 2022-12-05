e8f7631aed6e31379484c211e86e044da0851dc0
hapreet <harpreetneharyt@gmail.com>
2022-12-05 13:39:12 +0100

    -Add configuration

--- src/main/java/at/fhtw/swen3/configuration/AppConfig.java
+++ src/main/java/at/fhtw/swen3/configuration/AppConfig.java
@@ -1,7 +1,10 @@
 package at.fhtw.swen3.configuration;
 
 import at.fhtw.swen3.persistence.repositories.ParcelRepository;
+import at.fhtw.swen3.persistence.repositories.WarehouseRepository;
+import at.fhtw.swen3.services.WarehouseService;
 import at.fhtw.swen3.services.impl.ParcelServiceImpl;
+import at.fhtw.swen3.services.impl.WarehouseServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +14,11 @@ public class AppConfig {
     @Bean
     public ParcelServiceImpl parcelService(ParcelRepository parcelRepository) {
         return new ParcelServiceImpl(parcelRepository);
+    }
+
+    @Bean
+    public WarehouseServiceImpl parcelService(WarehouseRepository warehouseRepository) {
+        return new WarehouseServiceImpl(warehouseRepository);
     }
 
 }
