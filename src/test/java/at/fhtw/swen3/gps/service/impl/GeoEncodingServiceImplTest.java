fec390654f654e7d8a55642825bc27cab4de142a
mzn-b <71345937+mzn-b@users.noreply.github.com>
2022-12-23 14:25:37 +0100

    empty geoEncodingServiceImplTest

---
+++ src/test/java/at/fhtw/swen3/gps/service/impl/GeoEncodingServiceImplTest.java
@@ -0,0 +1,15 @@
+package at.fhtw.swen3.gps.service.impl;
+
+import at.fhtw.swen3.gps.service.GeoEncodingService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource("/application.properties")
+public class GeoEncodingServiceImplTest {
+    @Test
+    void emptyTest(){
+
+    }
+}
